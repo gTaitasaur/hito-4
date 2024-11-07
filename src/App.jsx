1e18e411dc3f05d5dec4acc599964972a97ed873
gtaitasaur <gtaitasaur@gmail.com>
2024-11-07 03:16:20 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import Home from './components/Home'
+//import Home from './components/Home'
 import Pizza from './components/Pizza'
 //import Cart from "./components/Cart";
 //import RegisterPage from './components/RegisterPage';
